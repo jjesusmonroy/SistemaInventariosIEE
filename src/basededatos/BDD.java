50548d150f3073a09b5be2e00d605c824ea0b590
pasto <pasto@10.58.1.147>
2017-10-26 23:03:22 -0700

    Creo que ya hice funcionar la validación del login

--- src/basededatos/BDD.java
+++ src/basededatos/BDD.java
@@ -5,10 +5,65 @@
  */
 package basededatos;
 
+import java.sql.*;
 /**
  *
  * @author pasto
  */
 public class BDD{
+
+    public BDD() {
+    }
+    
+    
+    
+    public static void main(String [] args){
+        
+        try{
+            
+            //1. Conectarse a la base de datos
+            Connection myConn = DriverManager.getConnection
+            ("jdbc:mysql://localhost:3306/dbis","root","root");
+            
+            //2. Crear una declaracion
+            Statement myStmt = myConn.createStatement();
+            
+            //3. Codigo en SQL
+            ResultSet myRs = myStmt.executeQuery("select * from usuarios where nombre_usuario = 'jjesusmonroy'");
+            //4. Resultado
+            while (myRs.next()){
+                System.out.println(myRs.getString("nombre_usuario")+ ", " + myRs.getString("contraseña_usuario"));
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+    
+    //metodo para validar inicio de sesion de login
+    public String[] validarInicio(String usuario){
+        
+            String [] a = new String[2];
+        try{
+            //1. Conectarse a la base de datos
+            Connection myConn = DriverManager.getConnection
+            ("jdbc:mysql://localhost:3306/dbis","root","root");
+            //2. Crear una declaracion
+            Statement myStmt = myConn.createStatement();
+            //3. Codigo en SQL
+            ResultSet myRs = myStmt.executeQuery("select * from usuarios where nombre_usuario = '"+usuario+"'");
+            //4. Resultado
+            while(myRs.next()){
+            a[0]=myRs.getString("nombre_usuario");
+            a[1]=myRs.getString("password_usuario");
+            }
+            myConn.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return a;
+    }   
     
 }
